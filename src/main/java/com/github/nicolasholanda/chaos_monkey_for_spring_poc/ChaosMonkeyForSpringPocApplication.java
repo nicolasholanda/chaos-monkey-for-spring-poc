b9d325c13bc7eefb9a6404bf3cb6ca7a9af68e3c
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-08-20 08:56:38 -0300

    Initial Commit

---
+++ src/main/java/com/github/nicolasholanda/chaos_monkey_for_spring_poc/ChaosMonkeyForSpringPocApplication.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.chaos_monkey_for_spring_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaosMonkeyForSpringPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaosMonkeyForSpringPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nicolasholanda/chaos_monkey_for_spring_poc/ChaosMonkeyForSpringPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.chaos_monkey_for_spring_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaosMonkeyForSpringPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
